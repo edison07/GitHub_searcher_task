2b06972fb3577fa5b347d482780cd64765ec9786
Edison <edisonlin@pixnet.tw>
2022-03-06 19:45:17 +0800

    feat: 新增searcher bar

--- GitHub_searcher_task/GitHub_searcher_task/ViewController.swift
+++ GitHub_searcher_task/GitHub_searcher_task/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchBar.delegate = self
     }
 
 
 }
 
+extension ViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+    }
+}
