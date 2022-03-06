c701692a9c9da6a7b5a9c7c6f71474067bdceeaf
Edison <edisonlin@pixnet.tw>
2022-03-06 20:00:50 +0800

    feat: 新增API service

---
+++ GitHub_searcher_task/GitHub_searcher_task/APIService.swift
@@ -0,0 +1,38 @@
+//
+//  APIService.swift
+//  GitHub_Searcher
+//
+//  Created by edisonlin on 2022/3/4.
+//
+
+import Foundation
+import Alamofire
+
+class APIService {
+    
+    static let shared = APIService()
+    
+    func executeQuery<T>(url:String, method: HTTPMethod, parameters: Parameters?, headers: HTTPHeaders?, encoding: ParameterEncoding = JSONEncoding.default, completion: @escaping (Result<T, Error>) -> Void) where T: Codable {
+        AF.request(url,method: method,parameters: parameters,encoding: encoding, headers: headers).responseData(completionHandler: {response in
+            switch response.result{
+            case .success(let res):
+                if let code = response.response?.statusCode{
+                    switch code {
+                    case 200...299:
+                        do {
+                            completion(.success(try JSONDecoder().decode(T.self, from: res)))
+                        } catch let error {
+                            print(String(data: res, encoding: .utf8) ?? "nothing received")
+                            completion(.failure(error))
+                        }
+                    default:
+                        let error = NSError(domain: response.debugDescription, code: code, userInfo: response.response?.allHeaderFields as? [String: Any])
+                        completion(.failure(error))
+                    }
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        })
+    }
+}
